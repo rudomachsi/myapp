456fe50930870be98e77170631d0eace9030d8e8
Rudo Machisi <rudomachisi@gmail.com>
2025-12-15 18:42:10 +0200

    Removed the repeated word and the  button

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" className="img-fluid"/>
+        <img src={logo} className="App-logo" alt="logo img-fluid"/>
         <br/>
-        <a href="#"className="btn btn-primary shadow"></a>
-      
+        
       </header>
       
     </div>
