a40c4e8eacca540ab90135cbca0772195099d540
Rudo Machisi <rudomachisi@gmail.com>
2025-12-15 22:50:04 +0200

    added search bar from lesson 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from"./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo img-fluid"/>
-        <br/>
+        </header>
+        <main>
+          <Dictionary/>
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Rudo Machisi</small> 
+          </footer>
         
-      </header>
       
+      </div>
     </div>
   );
 }
