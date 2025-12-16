400cba9eb11d8b105d836e8f297ab9b0367877ce
Rudo Machisi <rudomachisi@gmail.com>
2025-12-17 01:06:47 +0200

    link updated

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -11,7 +11,7 @@ export default function Phonetic(props) {
 
   </div>
 );
-      <a href={props.phonetic.audio} target="_blank" rel="noopener">
+      <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
         Listen
       </a>
       <span className="text">{props.phonetic.text}</span>
